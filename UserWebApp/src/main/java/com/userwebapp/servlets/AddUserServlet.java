1eefdb4edb69404bdf98b64327d3fbedcd11a5c3
Pranathi99 <pranathi.ganti@gmail.com>
2023-03-15 16:54:23 +0530

    UserWebApp

---
+++ UserWebApp/src/main/java/com/userwebapp/servlets/AddUserServlet.java
@@ -0,0 +1,75 @@
+package com.userwebapp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addUserServlet")   //Exported war file to src/main/webapp folder, then it worked , update---actually
+//build automatically is turned off, turn it on, so that no need to export new war file everytime changes are done
+public class AddUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private Connection conn;
+    
+    public AddUserServlet() {
+        super();
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","root1234");
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String fname=request.getParameter("firstName");
+		String lname=request.getParameter("lastName");
+		String emailId=request.getParameter("emailId");
+		String password=request.getParameter("password");
+		
+		try (Statement st=conn.createStatement();){
+			int result=st.executeUpdate("insert into user values('"+fname+"', '"+lname+"', '"+emailId+"', '"+password+"')");
+			PrintWriter out=response.getWriter();
+			response.setContentType("text/html");
+			if(result>0)
+			{
+				out.println("<h1>User created in DB</h1>");
+			}
+			else
+			{
+				out.println("<h1>Error creating user</h1>");
+			}
+			out.println("<a href='index.html'>Home</a>");
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		try {
+			if(conn!=null)
+				conn.close();
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
+}
